460eb620e8fece25af2da2554ed687a5c379a6d7
DenysPrav <pravduk.kh@gmail.com>
2022-10-10 19:48:46 +0200

    All files

---
+++ src/main/java/pl/hirely/springboot/HirelySpringbootModul7Application.java
@@ -0,0 +1,13 @@
+package pl.hirely.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirelySpringbootModul7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HirelySpringbootModul7Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/hirely/springboot/HirelySpringbootModul7ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hirely.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirelySpringbootModul7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
